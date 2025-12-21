b5926345dbd6b2d11f1f33729ac13e4c27574a82
Kolganova Viktoriia <vikaikido@gmail.com>
2025-12-21 18:25:03 +0300

    fix6

--- bot/src/main.py
+++ bot/src/main.py
@@ -35,8 +35,6 @@ class BotApplication:
 
         self.monitor_task = asyncio.create_task(self.monitor_service.monitor_all_users())
 
-        await self.bot.delete_webhook(drop_pending_updates=True)
-
         await self.dp.start_polling(self.bot)
 
 
