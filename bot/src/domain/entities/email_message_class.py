ea11601371682d4d0ad5d388917d278301362100
Mariia Kashcheeva <mskascheeva@edu.hse.ru>
2025-12-24 01:27:45 +0300

    добавила классификацию в код email monitor service

--- bot/src/domain/entities/email_message_class.py
+++ bot/src/domain/entities/email_message_class.py
@@ -8,3 +8,16 @@ class EmailMessage:
         self.from_ = self.headers.get('From', 'Unknown')
         self.subject = self.headers.get('Subject', 'No Subject')
         self.snippet = message_data.get('snippet', '')
+        self.body = self._get_body(message_data)
+
+    def _get_body(self, message_data):
+        if 'parts' in message_data['payload']:
+            parts = message_data['payload']['parts']
+            for part in parts:
+                if part.get('mimeType') == 'text/plain' and 'data' in part['body']:
+                    import base64
+                    return base64.urlsafe_b64decode(part['body']['data']).decode('utf-8')
+        elif 'data' in message_data['payload']['body']:
+            import base64
+            return base64.urlsafe_b64decode(message_data['payload']['body']['data']).decode('utf-8')
+        return ''
